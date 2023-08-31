4e13650fa9e04047c733f9477f4987de06594d65
caren <carenoliveirasilvapontes@gmail.com>
2023-08-31 16:49:20 -0300

    feat:projeto finalizado com sucesso

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,58 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import { useState } from 'react';
 
 export default function Home() {
+  const [primeiroValor,setPrimeiroValor] = useState('');
+  const [segundoValor,setSegundoValor] = useState('');
+  const [resultado,setResultado] = useState(0);
+  const [metodo,setMetodo] = useState('');
+
+  function calculo(sinal:any){
+    if(sinal ==='soma'){
+      setResultado(Number(primeiroValor) + Number(segundoValor))
+    }
+    if(sinal ==='subtracao'){
+      setResultado(Number(primeiroValor) - Number(segundoValor))
+    }
+
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main style={{backgroundColor: '#fff',height:'700px'}}>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+       <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" 
+        value={primeiroValor} onChange = {(e)=> setPrimeiroValor(e.target.value)}/>
+      <TextField id="filled-basic" label="Filled" variant="filled" 
+        value={segundoValor} onChange = {(e)=> setSegundoValor(e.target.value)} />
+      <TextField id="standard-basic" label="Standard" variant="standard" 
+        value={resultado} />
+    </Box>
+    <FormControl fullWidth>
+    <InputLabel id="demo-simple-select-label">métodos</InputLabel>
+    <Select
+      labelId="demo-simple-select-label"
+      id="demo-simple-select"
+      label="métodos"
+      onChange = {(e)=> calculo(e.target.value)}
+    >
+      <MenuItem value={'soma'} >soma</MenuItem>
+      <MenuItem value={'subtracao'} >subtração</MenuItem>
+    </Select>
+    </FormControl>
     </main>
   )
 }
